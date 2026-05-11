9255d42868e5d2032fed0d54574395beee107619
Raj <rajsharma222547@gmail.com>
2026-05-11 16:45:27 +0530

    create and hash apikey

--- services/src/services/api-key.service.ts
+++ services/src/services/api-key.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { count, eq } from 'drizzle-orm';
 import { DRIZZLE_DB } from 'src/database/database.module';
 import { api_key } from 'src/database/schema';
+import * as crypto from 'crypto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class ApiKeyService {
@@ -9,6 +11,13 @@ export class ApiKeyService {
     @Inject(DRIZZLE_DB)
     private readonly db: any,
   ) {}
+
+  private generateApiKey(): {plainTextKey: string, hashedKey: string} {
+    const hashedKey = crypto.randomUUID().replace(/-/g, "");
+    const secret = crypto.randomBytes(32).toString('base64url');
+    const plainTextKey = `CYPH${hashedKey}_${secret}`;
+    return { plainTextKey, hashedKey };
+  }
 
   async createApiKey(userId: string) {
     //! Deafult 5 api keys Limit
@@ -19,10 +28,28 @@ export class ApiKeyService {
     if (result.count >= 5) {
       throw new Error('You have reached the maximum limit of 5 API keys');
     }
+    const { plainTextKey, hashedKey } = this.generateApiKey();
+    const hash = argon2.hash(plainTextKey, {
+      type: argon2.argon2id,
+      timeCost: 2,
+      memoryCost: 65536,
+      parallelism: 1,
+    });
+
+    const prefix = plainTextKey.substring(0, 18) + '...';
+
+    await this.db.insert(api_key).values({
+      id: hashedKey,
+      user_id: userId,
+      prefix,
+      value: hash,
+    });
+
+    return {key: plainTextKey }; 
   }
 
   async listApiKeys(userId: string) {}
-
+ b
   async deleteApiKey(userId: string) {}
 
   async regenerateApiKey(userId: string, id: string) {}
